4d518e43e8df30d4878bfa224756f9c7f60a28c5
Khamardarshit <khamarrdarshir@gmail.com>
2026-04-09 12:55:08 +0000

    done

--- Encapsulation/Parameterized Constructor.cpp
+++ Encapsulation/Parameterized Constructor.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 class ParameterizedConstructor {
 public:
-    int X; // 1. Declare the member variable
-    
-    // 2. Initialize the member variable in the constructor
+    int X; 
     ParameterizedConstructor(int x) { 
         X = x; 
     }
@@ -14,10 +12,8 @@ public:
 };
 
 int main() {
-    // 3. Create the object
     ParameterizedConstructor obj(10);
     
-    // 4. Access X using the object instance 'obj'
     cout << obj.X << endl; 
     
     return 0;
